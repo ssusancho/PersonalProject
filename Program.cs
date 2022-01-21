332946b561c339eba15be4a626fcd8fe084fc703
ssusancho <scho2024@chadwickschool.org>
2022-01-21 08:17:52 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scho_PersonalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
